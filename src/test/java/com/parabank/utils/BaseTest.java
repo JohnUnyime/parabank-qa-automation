0ef88df6e08f301ae6b801f8b1b980c3ed9d6c4d
John Unyime <johnunyime1043@gmail.com>
2026-09-14 21:59:03 +0100

    Add headless Chrome support for CI environment

--- src/test/java/com/parabank/utils/BaseTest.java
+++ src/test/java/com/parabank/utils/BaseTest.java
@@ -3,6 +3,7 @@ package com.parabank.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,14 +18,25 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Run headless automatically when CI=true (GitHub Actions sets this by default)
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(
                 Duration.ofSeconds(ConfigReader.getInt("implicitWaitSeconds")));
         wait = new WebDriverWait(driver, Duration.ofSeconds(ConfigReader.getInt("explicitWaitSeconds")));
         driver.get(ConfigReader.get("baseUrl"));
     }
-   
+
     protected void login() {
         com.parabank.pages.LoginPage loginPage = new com.parabank.pages.LoginPage(driver, wait);
         loginPage.enterUsername(ConfigReader.get("username"));
